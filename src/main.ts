eb3ea74392a3ece48748f8d9f91c68dacf46de1a
Jushoru <smirnov.kirill.kaduy@gmail.com>
2025-07-31 11:21:31 +0300

    fix: delete middleware

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express'; // Изменённый импорт
 import { AppModule } from './app.module';
 import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 import * as session from 'express-session';
-import { join } from 'path';
-import { StaticAssetsMiddleware } from './middlewares/static-assets.middleware';
 
 async function bootstrap(): Promise<void> {
-  // Изменяем тип на NestExpressApplication
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
-  // ===== Static Assets Configuration =====
-  app.use(new StaticAssetsMiddleware().use);
-
-  // Теперь useStaticAssets будет доступен
-  app.useStaticAssets(join(__dirname, '..', 'static'), {
-    setHeaders: (res) => {
-      res.set('X-Content-Type-Options', 'nosniff');
-    }
-  });
+  const app = await NestFactory.create(AppModule);
 
   // ===== CORS Configuration =====
   app.enableCors({
@@ -30,7 +16,6 @@ async function bootstrap(): Promise<void> {
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
-  // ===== Swagger Documentation =====
   const config = new DocumentBuilder()
     .setTitle('Users API')
     .setDescription('t1 camp form api')
@@ -42,7 +27,6 @@ async function bootstrap(): Promise<void> {
     SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
-  // ===== Middlewares =====
   app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
@@ -50,22 +34,17 @@ async function bootstrap(): Promise<void> {
 
   app.use(
     session({
-      secret: process.env.SESSION_SECRET || 't1_camp_js',
+      secret: 't1_camp_js',
       resave: false,
       saveUninitialized: false,
       cookie: {
         httpOnly: true,
         maxAge: 24 * 60 * 60 * 1000,
-        sameSite: 'lax',
-        secure: process.env.NODE_ENV === 'production',
       },
     }),
   );
 
-  // ===== Server Start =====
-  const port = process.env.PORT ?? 4000;
-  await app.listen(port);
-  console.log(`Application is running on: http://localhost:${port}`);
+  await app.listen(process.env.PORT ?? 4000);
 }
 
 bootstrap();
