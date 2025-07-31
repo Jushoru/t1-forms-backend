d563980c613127071462a5cd43b5ed6fbca804b0
Jushoru <smirnov.kirill.kaduy@gmail.com>
2025-07-31 11:42:36 +0300

    fix: test strict

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -35,7 +35,7 @@ export class AuthController {
     response.cookie(jwtConstants.cookieName, access_token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'none',
+      sameSite: 'strict',
       maxAge: 24 * 60 * 60 * 1000,
     });
 
